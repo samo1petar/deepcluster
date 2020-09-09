45b7d8340951ca5a7bb1974459d998ff5b2447ba
Petar Pavlovic <pero356@gmail.com>
2020-09-09 15:05:12 +0200

    [change] test.sh

--- test.sh
+++ test.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
 CUDA_VISIBLE_DEVICES=0 python test.py \
-  --data '/home/david/Datasets/Flickr/Flickr25K' \
+  --data 'path/to/dataset/like/Flickr25K' \
   --arch 'vgg16' \
   --nmb_cluster 1000 \
   --cluster_alg 'KMeans' \
   --batch 32 \
-  --resume '/home/david/Projects/deepcluster/experiments/exp_6/checkpoints/checkpoint_443.0.pth.tar' \
+  --resume 'path/to/checkpoint.pth.tar' \
   --sobel
-
-
-#  --resume '/home/david/Projects/deepcluster/deepcluster_models/vgg16/checkpoint.pth.tar'
-# '/home/david/Projects/deepcluster/experiments/exp_1/checkpoint.pth.tar'
-
-# 1. /home/david/Projects/deepcluster/experiments/exp_3/checkpoints/checkpoint_28.0.pth.tar
-# 2. /home/david/Projects/deepcluster/experiments/exp_3/checkpoint.pth.tar
